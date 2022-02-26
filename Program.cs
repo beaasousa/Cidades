da53087983baec0a0a25467499608f937396124a
Ana Beatriz Costa de Sousa <anacostasousa08@gmail.com>
2022-02-25 22:34:08 -0300

    finalizar o exercício

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Santos!");
+Console.WriteLine("São Vicente!");
+Console.WriteLine("Praia Grande!");
+Console.WriteLine("Cubatão!");
+Console.WriteLine("Guarujá!");
+Console.WriteLine("Caraguatatuba!");
+Console.WriteLine("Itanhaém!");
+Console.WriteLine("Ubatuba!");
+Console.WriteLine("São Sebastião!");
+Console.WriteLine("Peruíbe!");
+Console.WriteLine("Bertioga!");
+Console.WriteLine("Mongaguá!");
+Console.WriteLine("Ilhabela!");
+Console.WriteLine("Iguape!");
+Console.WriteLine("Cananéia!");
